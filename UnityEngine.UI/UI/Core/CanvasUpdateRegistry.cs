508483798f2cb237ac70c37dd84316a58bed75a4
聂红强 <36918549+NRatel@users.noreply.github.com>
2021-06-07 21:12:58 +0800

    CanvasUpdateRegistry 注释

--- UnityEngine.UI/UI/Core/CanvasUpdateRegistry.cs
+++ UnityEngine.UI/UI/Core/CanvasUpdateRegistry.cs
@@ -23,8 +23,8 @@ namespace UnityEngine.UI
     public interface ICanvasElement
     {
         // Rebuild the element for the given stage.
-        // 对给定 CanvasUpdate stage 重建元素。
-        // 参数"executing"：给定的 CanvasUpdate stage。
+        // 对指定 CanvasUpdate stage 重建元素。
+        // 参数"executing"：指定的 CanvasUpdate stage。
         void Rebuild(CanvasUpdate executing);
 
         // Get the transform associated with the ICanvasElement.
@@ -56,13 +56,12 @@ namespace UnityEngine.UI
 
         private readonly IndexedSet<ICanvasElement> m_LayoutRebuildQueue = new IndexedSet<ICanvasElement>();
         private readonly IndexedSet<ICanvasElement> m_GraphicRebuildQueue = new IndexedSet<ICanvasElement>();
-
+        
+        // 注册事件 Canvas.willRenderCanvases，用来触发更新。（这是更新/重建的源动力）
+        // Canvas.willRenderCanvases：https://docs.unity3d.com/cn/2020.1/ScriptReference/Canvas-willRenderCanvases.html
+        // 在即将开始 Canvas 渲染前调用的事件。这让您能够延时处理 /更新基于画布的元素，直到即将开始渲染它们时。  
         protected CanvasUpdateRegistry()
         {
-            // 注册事件 Canvas.willRenderCanvases，用来触发更新。（这是更新/重建的源动力）
-            // Canvas.willRenderCanvases：https://docs.unity3d.com/cn/2020.1/ScriptReference/Canvas-willRenderCanvases.html
-            //    在即将开始 Canvas 渲染前调用的事件。
-            //    这让您能够延时处理 /更新基于画布的元素，直到即将开始渲染它们时。  
             Canvas.willRenderCanvases += PerformUpdate; 
         }
 
@@ -78,6 +77,8 @@ namespace UnityEngine.UI
             }
         }
 
+        // 判断对象对于更新是否有效。
+        // 有效指：非null、是UnityObject、存活。（实际肯定是非null，因为已经在CleanInvalidItems排除了）
         private bool ObjectValidForUpdate(ICanvasElement element)
         {
             var valid = element != null;
@@ -89,6 +90,9 @@ namespace UnityEngine.UI
             return valid;
         }
 
+        // 清理无效项 无效指： null 对象 或 已销毁的对象。
+        // 1、将无效项从 LayoutRebuild 队列中移除。若对象已被销毁，则直接触发 LayoutComplete 事件。
+        // 2、将无效项从 GraphicRebuild 队列中移除。若对象已被销毁，则直接触发 GraphicUpdateComplete 事件。
         private void CleanInvalidItems()
         {
             // So MB's override the == operator for null equality, which checks
@@ -97,6 +101,10 @@ namespace UnityEngine.UI
             // this won't forward the == operator to the MB, but just check if the
             // interface is null. IsDestroyed will return if the backend is destroyed.
 
+            // MonoBehaviour 重写了 == 操作符。可以用 “== null” 来判断物体否被销毁。
+            // 因此，如果我们正在查看一个具体的 MonoBehaviour，用“== null”来判断物体否被销毁是没问题的。
+            // 但在本例中，我们正在查看一个 ICanvasElement 列表。这不会将 == 操作符转发到 MonoBehaviour，而只是检查该接口类型的对象是否为null。
+            // 所以，要调用 IsDestroyed() 方法来确定实现了接口 ICanvasElement 的物体是否被销毁。
             for (int i = m_LayoutRebuildQueue.Count - 1; i >= 0; --i)
             {
                 var item = m_LayoutRebuildQueue[i];
@@ -131,23 +139,29 @@ namespace UnityEngine.UI
         }
 
         private static readonly Comparison<ICanvasElement> s_SortLayoutFunction = SortLayoutList;
+
+        // 执行更新！
+        // 1、清理无效项。
+        // 2、执行 LayoutUpdate。 注意两层for循环的顺序，每个阶段所有物体执行完Rebuild()，才进入下一个更新阶段。
+        // 3、执行 Culling。
+        // 4、执行 GraphicUpdate。注意两层for循环的顺序，每个阶段所有物体执行完Rebuild()，才进入下一个更新阶段。
         private void PerformUpdate()
         {
-            UISystemProfilerApi.BeginSample(UISystemProfilerApi.SampleType.Layout);
-            CleanInvalidItems();
+            UISystemProfilerApi.BeginSample(UISystemProfilerApi.SampleType.Layout);     //性能采样。 疑问??? 为什么只有 SampleType.Layout，没有 SampleType.Render？
+            CleanInvalidItems();                //清理无效项
 
-            m_PerformingLayoutUpdate = true;
+            m_PerformingLayoutUpdate = true;    //标记为正在进行 LayoutUpdate（开始）。
 
-            m_LayoutRebuildQueue.Sort(s_SortLayoutFunction);
-            for (int i = 0; i <= (int)CanvasUpdate.PostLayout; i++)
+            m_LayoutRebuildQueue.Sort(s_SortLayoutFunction);            //按父子嵌套层数降序排序。
+            for (int i = 0; i <= (int)CanvasUpdate.PostLayout; i++)     //依次执行 Prelayout、Layout、PostLayout。
             {
-                for (int j = 0; j < m_LayoutRebuildQueue.Count; j++)
+                for (int j = 0; j < m_LayoutRebuildQueue.Count; j++)    //排序后的队列元素依次执行。
                 {
                     var rebuild = instance.m_LayoutRebuildQueue[j];
                     try
                     {
-                        if (ObjectValidForUpdate(rebuild))
-                            rebuild.Rebuild((CanvasUpdate)i);
+                        if (ObjectValidForUpdate(rebuild))              //检查对象更新有效性。
+                            rebuild.Rebuild((CanvasUpdate)i);           //执行元素的 Rebuild()。
                     }
                     catch (Exception e)
                     {
@@ -157,24 +171,25 @@ namespace UnityEngine.UI
             }
 
             for (int i = 0; i < m_LayoutRebuildQueue.Count; ++i)
-                m_LayoutRebuildQueue[i].LayoutComplete();
+                m_LayoutRebuildQueue[i].LayoutComplete();               //触发 LayoutComplete 事件。
 
-            instance.m_LayoutRebuildQueue.Clear();
-            m_PerformingLayoutUpdate = false;
+            instance.m_LayoutRebuildQueue.Clear();  //清空 LayoutRebuild 队列。
+            m_PerformingLayoutUpdate = false;       //标记为不在进行 LayoutUpdate（结束）。
 
             // now layout is complete do culling...
+            // 现在，layout结束了，执行剔除逻辑。
             ClipperRegistry.instance.Cull();
 
-            m_PerformingGraphicUpdate = true;
-            for (var i = (int)CanvasUpdate.PreRender; i < (int)CanvasUpdate.MaxUpdateValue; i++)
+            m_PerformingGraphicUpdate = true;   //标记为正在进行 GraphicUpdate（开始）。
+            for (var i = (int)CanvasUpdate.PreRender; i < (int)CanvasUpdate.MaxUpdateValue; i++)    //依次执行 PreRender、LatePreRender。
             {
-                for (var k = 0; k < instance.m_GraphicRebuildQueue.Count; k++)
+                for (var k = 0; k < instance.m_GraphicRebuildQueue.Count; k++)  //队列元素依次执行。
                 {
                     try
                     {
                         var element = instance.m_GraphicRebuildQueue[k];
-                        if (ObjectValidForUpdate(element))
-                            element.Rebuild((CanvasUpdate)i);
+                        if (ObjectValidForUpdate(element))                      //检查对象更新有效性。                        
+                            element.Rebuild((CanvasUpdate)i);                   //执行元素的 Rebuild()。
                     }
                     catch (Exception e)
                     {
@@ -184,13 +199,14 @@ namespace UnityEngine.UI
             }
 
             for (int i = 0; i < m_GraphicRebuildQueue.Count; ++i)
-                m_GraphicRebuildQueue[i].GraphicUpdateComplete();
+                m_GraphicRebuildQueue[i].GraphicUpdateComplete();               //触发 GraphicUpdateComplete 事件。
 
-            instance.m_GraphicRebuildQueue.Clear();
-            m_PerformingGraphicUpdate = false;
+            instance.m_GraphicRebuildQueue.Clear();     //清空 GraphicRebuildQueue 队列。
+            m_PerformingGraphicUpdate = false;          //标记为不在进行 GraphicUpdate（结束）。
             UISystemProfilerApi.EndSample(UISystemProfilerApi.SampleType.Layout);
         }
 
+        //取父物体数量（父子嵌套层数）
         private static int ParentCount(Transform child)
         {
             if (child == null)
@@ -206,6 +222,8 @@ namespace UnityEngine.UI
             return count;
         }
 
+        // 对 LayoutList 排序的比较方法。
+        // 按父物体数量（父子嵌套层数）降序。
         private static int SortLayoutList(ICanvasElement x, ICanvasElement y)
         {
             Transform t1 = x.transform;
@@ -214,37 +232,36 @@ namespace UnityEngine.UI
             return ParentCount(t1) - ParentCount(t2);
         }
 
-        /// <summary>
-        /// Try and add the given element to the layout rebuild list.
-        /// Will not return if successfully added.
-        /// </summary>
-        /// <param name="element">The element that is needing rebuilt.</param>
+        // Try and add the given element to the layout rebuild list.
+        // Will not return if successfully added. 
+        // 尝试将指定元素添加到 LayoutRebuild 队列中。
+        // 添加成功无返回值。
+        // 参数 "element"：The element that is needing rebuilt. 需要重建的元素。
         public static void RegisterCanvasElementForLayoutRebuild(ICanvasElement element)
         {
             instance.InternalRegisterCanvasElementForLayoutRebuild(element);
         }
 
-        /// <summary>
-        /// Try and add the given element to the layout rebuild list.
-        /// </summary>
-        /// <param name="element">The element that is needing rebuilt.</param>
-        /// <returns>
-        /// True if the element was successfully added to the rebuilt list.
-        /// False if either already inside a Graphic Update loop OR has already been added to the list.
-        /// </returns>
+        // Try and add the given element to the layout rebuild list.
+        //"element"：The element that is needing rebuilt. 需要重建的元素。
+        // True if the element was successfully added to the rebuilt list.
+        // False if either already inside a Graphic Update loop OR has already been added to the list.
+        // 尝试将指定元素添加到 LayoutRebuild 队列中。
+        // 若添加成功返回true，若正在执行更新或已存在于 LayoutRebuild 队列中则返回false。
         public static bool TryRegisterCanvasElementForLayoutRebuild(ICanvasElement element)
         {
             return instance.InternalRegisterCanvasElementForLayoutRebuild(element);
         }
 
         // 内部私有，添加 LayoutRebuild 注册
-        // 1、检查确保不在 LayoutRebuild 队列中。 //注意：这里未使用 m_PerformingLayoutUpdate 判断。因为在调整游戏视图大小时会导致显示错误。
+        // 1、检查确保不在 LayoutRebuild 队列中。 //注意：这里未使用 m_PerformingLayoutUpdate 判断。因为有个待处理 bug。
         // 2、加入 LayoutRebuild 队列。
         private bool InternalRegisterCanvasElementForLayoutRebuild(ICanvasElement element)
         {
             if (m_LayoutRebuildQueue.Contains(element))
                 return false;
-            
+
+            // 待处理 bug：在调整游戏视图大小时会导致显示错误。
             /* TODO: this likely should be here but causes the error to show just resizing the game view (case 739376)
             if (m_PerformingLayoutUpdate)
             {
@@ -255,23 +272,23 @@ namespace UnityEngine.UI
             return m_LayoutRebuildQueue.AddUnique(element);
         }
 
-       
-        // Try and add the given element to the rebuild list. Will not return if successfully added.
-        // 
+
+        // Try and add the given element to the rebuild list.
+        // Will not return if successfully added. 
+        // 尝试将指定元素添加到 GraphicRebuild 队列中。
+        // 添加成功无返回值。
         // 参数 "element"：The element that is needing rebuilt. 需要重建的元素。
         public static void RegisterCanvasElementForGraphicRebuild(ICanvasElement element)
         {
             instance.InternalRegisterCanvasElementForGraphicRebuild(element);
         }
 
-        /// <summary>
-        /// Try and add the given element to the rebuild list.
-        /// </summary>
-        /// <param name="element">The element that is needing rebuilt.</param>
-        /// <returns>
-        /// True if the element was successfully added to the rebuilt list.
-        /// False if either already inside a Graphic Update loop OR has already been added to the list.
-        /// </returns>
+        // Try and add the given element to the rebuild list.
+        //"element"：The element that is needing rebuilt. 需要重建的元素。
+        // True if the element was successfully added to the rebuilt list.
+        // False if either already inside a Graphic Update loop OR has already been added to the list.
+        // 尝试将指定元素添加到 GraphicRebuild 队列中。
+        // 若添加成功返回true，若正在执行更新或已存在于 GraphicRebuild 队列中则返回false。
         public static bool TryRegisterCanvasElementForGraphicRebuild(ICanvasElement element)
         {
             return instance.InternalRegisterCanvasElementForGraphicRebuild(element);
@@ -291,10 +308,9 @@ namespace UnityEngine.UI
             return m_GraphicRebuildQueue.AddUnique(element);
         }
 
-        /// <summary>
-        /// Remove the given element from both the graphic and the layout rebuild lists.
-        /// </summary>
-        /// <param name="element"></param>
+        // Remove the given element from both the graphic and the layout rebuild lists.
+        // 将指定元素从 LayoutRebuild 队列 和 GraphicRebuild 队列中移除。
+        // 参数 "element"：需要移除的元素。
         public static void UnRegisterCanvasElementForRebuild(ICanvasElement element)
         {
             instance.InternalUnRegisterCanvasElementForLayoutRebuild(element);
